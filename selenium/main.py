eb025b910a28ee0f90862266ae2d49bda0f67bec
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2025-10-28 10:13:49 -0300

    automation

--- selenium/main.py
+++ selenium/main.py
@@ -1,12 +1,20 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+from dotenv import load_dotenv
 
-service = Service(executable_path="chromedriver.exe")
+load_dotenv()
+driver_path = os.path.join(os.path.dirname(__file__), "drivers", "chromedriver.exe")
+
+
+# Access variables
+login_url = os.getenv("LOGIN_URL")
+
+service = Service(executable_path=os.path.abspath(driver_path))
+
 driver = webdriver.Chrome(service=service)
-
-driver.get("http://localhost:3000/login")
+driver.get(login_url)
 
 time.sleep(5)
-
 driver.quit()
